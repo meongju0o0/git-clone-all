a2fc32b4b98751385690239f6e64d1856da03396
meongju0o0 <bl4angel20@khu.ac.kr>
2023-09-17 16:54:48 +0900

    add ignore, modify linux

--- linux-bash/clone-repos.sh
+++ linux-bash/clone-repos.sh
@@ -1,26 +1,42 @@
-@echo off
-setlocal enabledelayedexpansion
-set USERNAME=your-username
-set PAGE=1
+#!/bin/zsh
 
-:loop
-for /f "delims=" %%i in ('curl -s "https://api.github.com/users/%USERNAME%/repos?page=%PAGE%&per_page=100" ^| jq -r ".[].clone_url"') do (
-    set REPO_NAME=%%~ni
-    if exist !REPO_NAME! (
-        echo Updating existing repository: !REPO_NAME!
-        cd !REPO_NAME!
-        git fetch origin
-        git diff --exit-code || echo Warning: Local changes detected in !REPO_NAME! - skipping pull
-        git diff --quiet && git pull origin master
-        cd ..
-    ) else (
-        echo Cloning new repository: !REPO_NAME!
-        git clone %%i
-    )
-)
+USERNAME="meongju0o0"
+PAGE=1
 
-set /a PAGE=%PAGE%+1
-curl -I -s "https://api.github.com/users/%USERNAME%/repos?page=%PAGE%&per_page=100" | find "HTTP/1.1 200" > nul
-if %errorlevel%==0 goto loop
+while true; do
+    REPO_URLS=$(curl -s "https://api.github.com/users/$USERNAME/repos?page=$PAGE&per_page=100" | jq -r ".[].clone_url")
 
-endlocal
+    # Check if the response is empty, if so, break out of the loop
+    if [[ -z "$REPO_URLS" ]]; then
+        break
+    fi
+
+    for REPO_URL in ${(f)REPO_URLS}; do
+        REPO_NAME=$(basename -s .git $REPO_URL)
+        if [[ -d $REPO_NAME ]]; then
+            echo "Updating existing repository: $REPO_NAME"
+            cd $REPO_NAME
+            git fetch origin
+            
+            # Check for local changes and either pull or show a warning
+            if git diff --quiet; then
+                git pull origin master
+            else
+                echo "Warning: Local changes detected in $REPO_NAME - skipping pull"
+            fi
+
+            cd ..
+        else
+            echo "Cloning new repository: $REPO_NAME"
+            git clone $REPO_URL
+        fi
+    done
+
+    # Increment the page number and check if there's a next page
+    PAGE=$((PAGE+1))
+    STATUS_CODE=$(curl -I -s "https://api.github.com/users/$USERNAME/repos?page=$PAGE&per_page=100" | grep "HTTP/2" | awk '{print $2}')
+    
+    if [[ "$STATUS_CODE" -ne 200 ]]; then
+        break
+    fi
+done
