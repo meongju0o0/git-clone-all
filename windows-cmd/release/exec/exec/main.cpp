423772cc8be7178568eaca1d0e006347cf412e19
meongju0o0 <bl4angel20@gmail.com>
2023-11-21 07:23:01 +0900

    add windows release

---
+++ windows-cmd/release/exec/exec/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	string username;
+	cout << "Enter Username:";
+	cin >> username;
+
+	int pkg_result = system("pkg-install.ps1");
+	int clone_result = system("clone-repos.bat");
+	
+	return EXIT_SUCCESS;
+}
