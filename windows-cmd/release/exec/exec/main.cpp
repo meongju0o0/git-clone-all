7d5e3e033146d23bf5a0c4f6192954d1f644713b
meongju0o0 <bl4angel20@gmail.com>
2023-11-21 17:04:25 +0900

    .

--- windows-cmd/release/exec/exec/main.cpp
+++ windows-cmd/release/exec/exec/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char* argv[]) {
 	string username;
 	cout << "Enter Username:";
 	cin >> username;
+	cout << endl;
 
 	int pkg_result = system("pkg-install.ps1");
 	int clone_result = system("clone-repos.bat");
